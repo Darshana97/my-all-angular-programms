d5a56d672f39d9bcdbfaa500ef9805f43501d076
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-01-25 07:22:56 +0530

    propert binding in angular

---
+++ property binding/src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'property-and-attribute';
+  public name = "Achi";
+  public myId = "testId";
+  public isDisabled = true;
+
+  
+}
